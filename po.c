722fcd06a3feb4d3abd5edffd6eaac51d4980ad4
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-01-26 09:49:01 +0530

    Create po.c

---
+++ po.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("Enter a number to find whether it is odd or even");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("Even");
+}
+else
+{
+printf("Odd");
+}
+}
+© 2018 GitHub, Inc.
